4e413ad747531fa73c0b6ab58c7e0a1b876b607f
LukeSlater02 <lukeslater101@gmail.com>
2022-02-17 11:23:26 -0600

    v1

---
+++ chapter3-deconstruction.js
@@ -0,0 +1,55 @@
+// Task: Go to the Gym
+const getReady = () => {
+    console.log("Code to select clothes and put them on, get water bottle, etc.");
+}
+
+const driveToGym = () => {
+    console.log("Code to interact with the car object.");
+}
+
+const stretch = () => {
+    console.log("Code to stretch.");
+}
+
+const selectMusic = () => {
+    console.log("Code to select the music I want to listen to.");
+}
+
+const benchPress = () => {
+    console.log("Code to perform a bench press.")
+}
+
+const benchPress = () => {
+    console.log("Code to perform a bench press.")
+}
+
+const dumbbellCurl = () => {
+    console.log("Code to perform a dumbbell curl.")
+}
+
+const cableRow = () => {
+    console.log("Code to perform a cable row.");
+}
+
+const shower = () => {
+    console.log("Code to take a shower.");
+}
+
+const driveHome = () => {
+    console.log("Code to interact with the car object.");
+}
+
+const eat = () => {
+    console.log("Code to eat food.")
+}
+
+getReady();
+driveToGym();
+stretch();
+selectMusic();
+benchPress();
+cableRow();
+dumbbellCurl();
+shower();
+driveHome();
+eat();
